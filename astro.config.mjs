c98059f6f368c4828fc96221b8197a706121a8cb
ror5zg <frunofkahi@gmail.com>
2025-04-13 11:24:46 +0900

    tailwind追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
       svg: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
